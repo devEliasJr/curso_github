92d583f278895c3250a484192dffae6e7c2d9181
develias <eliasengmecanica@gmail.com>
2023-01-17 10:43:05 -0300

    Adicionando mensagem de boas vindas

--- cadastro.js
+++ cadastro.js
@@ -1,7 +1,13 @@
 function singUp(nome, email) {
     return nome, email;
 }
+
+// pegando sobrenome e emailsecundário
 function singUp(sobrenome, jemail) {
     return sobrenome, jemail;
 }
 
+//adicionando a funcao de mensagem
+function welcome(nome) {
+    return 'Welcome', nome;
+}
